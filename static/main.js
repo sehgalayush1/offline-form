63242983437b2c8bbc6eb54b98ecf1b8f36a69f5
Ayush Sehgal <sehgalayush26@gmail.com>
2016-06-24 14:41:28 +0530

    indb edited

--- static/main.js
+++ static/main.js
@@ -9,53 +9,68 @@ if ('serviceWorker' in navigator) {
 }
 
 
-$(document).ready(function(){
-                window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-                var request, db; 
+function sendForm() {
+    return addFormToOutbox().then(() => {
+        return navigator.serviceWorker.ready;
+    }).then(reg => {
+        return reg.sync.register('send-form');
+    }).then(() => {
+        console.log('Sync registered!');
+    }).catch(() => {
+        console.log('Sync registration failed :(');
+    });
+}
+
+
+function addFormToOutbox() {
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    var request, db; 
                 
-                if(!window.indexedDB)
-                {
-                    console.log("Your Browser does not support IndexedDB");
-                }
-                else
-                {
-                    request = window.indexedDB.open("Form", 2);
-                    request.onerror = function(event){
-                        console.log("Error opening DB", event);
-                    }
-                    request.onupgradeneeded   = function(event){
-                        console.log("Upgrading");
-                        db = event.target.result;
-                        var objectStore = db.createObjectStore("form_data", { keyPath : "email" });
-                    };
-                    request.onsuccess  = function(event){
-                        console.log("Success opening DB");
-                        db = event.target.result;
-                    }
-                }
+    if(!window.indexedDB)
+    {
+        console.log("Your Browser does not support IndexedDB");
+    }
+    else
+    {
+        request = window.indexedDB.open("Form", 2);
+        request.onerror = function(event){
+            console.log("Error opening DB", event);
+        }
+        request.onupgradeneeded   = function(event){
+            console.log("Upgrading");
+            db = event.target.result;
+            var objectStore = db.createObjectStore("form_data", { keyPath : "email" });
+        };
+        request.onsuccess  = function(event){
+            console.log("Success opening DB");
+            db = event.target.result;
+        }
+    }
                 
-                $("#btn").click(function(){
-                    var email = $("#email").val();
-					var pswd = $("#pswd").val();
-					var cpswd = $("#cpswd").val();
-					var tw = $("#tw").val();
-					var fb = $("#fb").val();
-					var gp = $("#gp").val();
-					var fn = $("#fn").val();
-					var ln = $("#ln").val();
-					var pn = $("#pn").val();
-					var ad = $("#ad").val();
+    $("#btn").click(function(){
+        var email = $("#email").val();
+        var pswd = $("#pswd").val();
+        var cpswd = $("#cpswd").val();
+        var tw = $("#tw").val();
+        var fb = $("#fb").val();
+        var gp = $("#gp").val();
+        var fn = $("#fn").val();
+        var ln = $("#ln").val();
+        var pn = $("#pn").val();
+        var ad = $("#ad").val();
                     
-                    var transaction = db.transaction(["form_data"],"readwrite");
-                    transaction.oncomplete = function(event) {
-                        console.log("Success :)");
-                    };
+        var transaction = db.transaction(["form_data"],"readwrite");
+        transaction.oncomplete = function(event) {
+            console.log("Success :)");
+        };
                     
-                    transaction.onerror = function(event) {
-                        console.log("Error :(");
-                    };  
-                    var objectStore = transaction.objectStore("form_data");
+        transaction.onerror = function(event) {
+            console.log("Error :(");
+        };  
+        var objectStore = transaction.objectStore("form_data");
                     
-                    objectStore.add({email: email, pswd: pswd, cpswd: cpswd, tw: tw, fb: fb, gp: gp, fn: fn, ln: ln, pn: pn, ad: ad});
-                });
-});
+        objectStore.add({email: email, pswd: pswd, cpswd: cpswd, tw: tw, fb: fb, gp: gp, fn: fn, ln: ln, pn: pn, ad: ad});
+    });
+}
+
+
--- static/sw.js
+++ static/sw.js
@@ -6,3 +6,19 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Activated', event);
 });
+
+self.addEventListener('sync', event => {
+	if (event.tag == 'send-form') {
+		event.waitUntil(
+			getFormFromOutbox().then(form => {
+				return fetch('/send', {
+					method: 'POST',
+					body: JSON.stringify(form),
+					headers: { 'Content-Type': 'application/json' }
+				}).then(() => {
+					return removeFormFromOutbox(form);
+				});
+			})
+		);
+	}
+});
